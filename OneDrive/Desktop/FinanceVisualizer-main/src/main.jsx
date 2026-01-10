e0df8a1df7d0b405946d17f8221aff3e02e8c0b3
Prerna Preyshi <prernapreyshi2105@gmail.com>
2026-01-11 00:15:34 +0530

    Update the UI

---
+++ OneDrive/Desktop/FinanceVisualizer-main/src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+
+createRoot(rootElement).render(
+  <App />
+);
